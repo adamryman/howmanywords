d69a088fbff461d764ea845965c23ee309620a11
Adam Ryman <adamryman@gmail.com>
2014-11-09 08:02:39 -0800

    Removed parts that did not work, also changed a to input

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var words = {};
 var count = 0;
 function enterInput(){
    if(event.keyCode == 13 && document.getElementById('input').value != "") {
-      var a = document.getElementById('input').value;
-      if(words[a]){
+      var input = document.getElementById('input').value;
+      if(words[input]){
          document.getElementById('input').value = "";
          alert("YOU FAIL");
          document.getElementById('words').innerHTML = JSON.stringify(words);
       } else {
-         words[a] = true;
+         words[input] = true;
          count++;
          document.getElementById('words').innerHTML = count;
          document.getElementById('input').value = "";
