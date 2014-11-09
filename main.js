db2b1cf16444dbe301868ba6ef327d7f8c596eaf
Adam Ryman <adamryman@gmail.com>
2014-11-09 08:03:01 -0800

    comments

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var words = {};
 var count = 0;
+//comments
 function enterInput(){
    if(event.keyCode == 13 && document.getElementById('input').value != "") {
       var input = document.getElementById('input').value;
       if(words[input]){
+         //comments
          document.getElementById('input').value = "";
          alert("YOU FAIL");
          document.getElementById('words').innerHTML = JSON.stringify(words);
       } else {
+         //comments
          words[input] = true;
          count++;
          document.getElementById('words').innerHTML = count;
